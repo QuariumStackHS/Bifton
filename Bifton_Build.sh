dcc8e6bd97adcda4034bb40c4ae6875e3bb3565b
slowriding <48739803+slowriding@users.noreply.github.com>
2022-03-01 01:34:57 -0500

    Commiting

--- Bifton_Build.sh
+++ Bifton_Build.sh
@@ -1,2 +1,7 @@
+gcc -c -o./src/SQLite3/sqlite3.o ./src/SQLite3/sqlite3.c -std=c17
+g++ -c -o./src/BiftonCxx/BiftonCxx.o ./src/BiftonCxx/BiftonCxx.cpp -std=c++17
+g++ -c -o./src/BiftonCxx/SHA1/SHA1.o ./src/BiftonCxx/SHA1/SHA1.cpp -std=c++17
+g++ -c -o./src/SimpleExecuter.o ./src/SimpleExecuter.cpp -std=c++17
+g++ -c -o./src/Bifton.o ./src/Bifton.cpp -std=c++17
 gcc -lstdc++ ./src/SQLite3/sqlite3.o ./src/BiftonCxx/BiftonCxx.o ./src/BiftonCxx/SHA1/SHA1.o ./src/SimpleExecuter.o ./src/Bifton.o  -O3  -oBifton
 
